cc389b6e9085c0fbe7a0723cc4aa2732df565c87
dumdum12345-cyper <ugwokechidubem56@gmail.com>
2026-03-09 15:01:38 +0530

    Fix app.py syntax errors

--- app.py
+++ app.py
@@ -83,6 +83,5 @@ if __name__ == "__main__":
     print("  GITAM Student Helpdesk")
     print("  Open: http://127.0.0.1:5000")
     print("=" * 50)
-   if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(debug=False, host='0.0.0.0', port=port)
